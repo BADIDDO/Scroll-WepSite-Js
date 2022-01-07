44944760ed0d9c97ab3b4fc91f4ce4ba816b91d7
BADIDDO <aymenmatrix311@gmail.com>
2022-01-07 17:51:22 +0100

    update

---
+++ app.js
@@ -0,0 +1,9 @@
+ScrollReveal({
+    reset: true ,
+    distance : '60px',
+    duration : 2500,
+    delay : 400,
+     });
+     ScrollReveal().reveal('h1' , {delay : 500 , origin : 'left'});
+     ScrollReveal().reveal('.img1 , .content' , {delay : 600 , origin : 'bottom'});
+     ScrollReveal().reveal('.icon-social i' , {delay : 500 , origin : 'bottom' , interval : 200});
